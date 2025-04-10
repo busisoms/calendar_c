de20b512e26f98f9732358b3d0c3b49f0a29d4e8
Busiso <busisondumo1@gmail.com>
2025-04-10 23:51:42 +0200

    Update: header file with new prototypes

--- cal.h
+++ cal.h
@@ -4,10 +4,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
 
 char **tokenize(char *string);
 unsigned int get_date(char **date);
-
+int is_4_digits(char *str);
+int get_current_year(void);
+int is_month(char *str);
+void error_msg(const char *message);
+int *validate(char **tokens);
 
 #endif
 
