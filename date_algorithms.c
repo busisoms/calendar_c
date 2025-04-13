b9b853714ff66c87b0a4b59aaa19c31e6bde3a1e
Busiso <busisondumo1@gmail.com>
2025-04-13 17:16:11 +0200

    Feat: add function to calculate the number of days in a given month

--- date_algorithms.c
+++ date_algorithms.c
@@ -22,6 +22,37 @@ int is_leap_year(int year)
 	return (0);
 }
 
+/**
+ * number_of_days - returns number of day according to month
+ * @mon: month
+ * @yr: year
+ *
+ * Return: number of days in a month (MIN 28 - MAX 31)
+ */
+
+int number_of_days(int mon, int yr)
+{
+	switch (mon)
+	{
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			return (31);
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			return (30);
+		case 2:
+			return ((is_leap_year(yr) == 1) ? 29 : 28);
+		default:
+			return (-1);
+	}
+}
 
 /**
  * day_of_week - calculates the day of the week using Zeller's
